013d27c54a800068ca2e4ff8dda60b34e4180cee
unknown <3911755@myuwc.ac.za>
2023-09-14 15:33:24 +0200

    first comm

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home container min-h-100">
+    <h4>WorkServices</h4>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">employeeID</th>
+          <th scope="col">PersonID</th>
+          <th scope="col">employeeNum</th>
+          <th scope="col">startDate</th>
+          <th scope="col">CTermination</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>5823</td>
+          <td>1020</td>
+          <td>Mark</td>
+          <td>2015-06-01</td>
+          <td>unknown</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>5824</td>
+          <td>1030</td>
+          <td>Jacob</td>
+          <td>2011-08-01</td>
+          <td>unknown</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>5825</td>
+          <td>1040</td>
+          <td>Larry</td>
+          <td>2018-05-30</td>
+          <td>unknown</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container min-h-100">
+    <h4>Employees</h4>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">PersonID</th>
+          <th scope="col">Firstname</th>
+          <th scope="col">Lastname</th>
+          <th scope="col">DateOfbirth</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>1020</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>1975-04-02</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>1030</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>1990-08-28</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>1040</td>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>1986-04-20</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
